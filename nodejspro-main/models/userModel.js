f84330a84cb8aafcb77e21c439ad63ffe7f92583
Hemanth Kumar <hemanthkumar6393142@gmail.com>
2025-06-25 19:39:13 +0530

    Delete nodejspro-main directory

--- nodejspro-main/models/userModel.js
+++
@@ -1,9 +0,0 @@
-const db = require('../config/db');
-
-exports.getAllUsers = (callback) => {
-  db.query('SELECT * FROM users', callback);
-};
-
-exports.createUser = (user, callback) => {
-  db.query('INSERT INTO users SET ?', user, callback);
-};
